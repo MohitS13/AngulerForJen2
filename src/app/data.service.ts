e2668802f1ff50f3c8410857561bcc97fce9b69f
mohit <mohit@gmail.com>
2019-11-28 18:11:01 +0530

    ip changed from AWS prac1

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,28 +8,28 @@ export class DataService {
   
   constructor( private helper : HttpClient) { console.log("data service obj created.. "); }
   Delete(No) {
-    return this.helper.delete("http://52.66.87.207:9898/emps/"+No);
+    return this.helper.delete("http://localhost:9898/emps/"+No);
   }
 
   Insert(empObj)
   { 
-    return this.helper.post("http://52.66.87.207:9898/emps",empObj);
+    return this.helper.post("http://localhost:9898/emps",empObj);
   }
 
  
 
   Select()
   {
-    return this.helper.get("http://52.66.87.207:9898/emps/111");
+    return this.helper.get("http://localhost:9898/emps/111");
   }
 
   SelectByNo(No)
   {
-    return this.helper.get("http://52.66.87.207:9898/emps/"+No);
+    return this.helper.get("http://localhost:9898/emps/"+No);
   }
 
   Update(emp) 
   {
-    return this.helper.put("http://52.66.87.207:9898/emps/"+emp.No,emp);
+    return this.helper.put("http://localhost:9898/emps/"+emp.No,emp);
   }
 }
